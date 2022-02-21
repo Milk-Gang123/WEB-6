0d7b65b1e0b27c50c68bddc6c7e6b741f907dfa1
Администратор <alexushveridze@mail.ru>
2022-02-21 14:37:30 +0300

    fix

--- Угадай-ка город.py
+++ Угадай-ка город.py
@@ -30,7 +30,7 @@ for i in cities:
     response = requests.get(search_api_server, params=search_params)
     responses.append(response)
 
-map_file = "map.png"
+map_file = "img/map.png"
 with open(map_file, "wb") as file:
     response = responses[k]
     file.write(response.content)
